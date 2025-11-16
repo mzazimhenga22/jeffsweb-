64cf96be07571e8a9965a17417c00f384d9d5680
Mzaziforex <mzaziforex@gmail.com>
2025-11-16 20:38:58 +0000

    Try fixing this error: `Build Error: Export Pinterest doesn't exist in t

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import Image from 'next/image';
-import { Star, Check, Minus, Plus, Loader, Heart, Facebook, Twitter, Pinterest, Share2 } from 'lucide-react';
+import { Star, Check, Minus, Plus, Loader, Heart, Facebook, Twitter, Share2 } from 'lucide-react';
 import * as React from 'react';
 
 import { MainLayout } from '@/components/main-layout';
@@ -277,7 +277,11 @@ export default function ProductDetailPage({
                     <Share2 className='text-muted-foreground' />
                     <Button variant="ghost" size="icon"><Facebook className="h-5 w-5 text-muted-foreground hover:text-primary" /></Button>
                     <Button variant="ghost" size="icon"><Twitter className="h-5 w-5 text-muted-foreground hover:text-primary" /></Button>
-                    <Button variant="ghost" size="icon"><Pinterest className="h-5 w-5 text-muted-foreground hover:text-primary" /></Button>
+                    <Button variant="ghost" size="icon">
+                        <svg className="h-5 w-5 text-muted-foreground hover:text-primary" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path fillRule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.237 2.636 7.855 6.356 9.312-.088-.791-.167-2.005.035-2.868.182-.78 1.172-4.97 1.172-4.97s-.299-.6-.299-1.486c0-1.39.806-2.428 1.81-2.428.852 0 1.264.64 1.264 1.408 0 .858-.545 2.14-.828 3.33-.236.995.5 1.807 1.48 1.807 1.778 0 3.144-1.874 3.144-4.58 0-2.393-1.72-4.068-4.177-4.068-2.845 0-4.515 2.135-4.515 4.34 0 .859.331 1.781.745 2.281a.3.3 0 01.069.288l-.278 1.133c-.044.183-.145.223-.335.134-1.249-.581-2.03-2.407-2.03-3.874 0-3.154 2.292-6.052 6.608-6.052 3.469 0 6.165 2.473 6.165 5.776 0 3.447-2.173 6.22-5.19 6.22-1.013 0-1.965-.527-2.291-1.148l-.623 2.378c-.226.869-.835 1.958-1.244 2.621.937.29 1.905.448 2.91.448 5.523 0 10-4.477 10-10S17.523 2 12 2z" clipRule="evenodd" />
+                        </svg>
+                    </Button>
                 </div>
             </Card>
 
